34b2a8fe24eb84be6c1611e114ec54ffa2b8cebf
Logan Woolf <loganwoolf@gmail.com>
2024-10-07 23:24:12 -0600

    Set up guess input prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Guess the Number!");
+
+    println!("Make your first guess: ");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read guess");
+
+    println!("Your guess: {}", guess);
 }
